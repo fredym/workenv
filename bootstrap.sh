fa4256390f05d655f06b6b4eebeebe875ded33e8
Fredy Muñoz <fredy@munoz.im>
2022-06-24 18:52:48 -0600

    Transfer to boostrap.sh the code that needs to run only once

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,6 +49,46 @@ else
 fi
 
 
+# Symlink and download useful tools and files
+log "Symlinking useful tools and files..."
+ln -sf /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+
+
+# Install config files
+log "Installing config files..."
+
+
+# Create symlink in homedir to config directory
+# This helps having short paths
+ln -sfh "$workenv_path"/config $HOME/.dot
+
+
+# Replace config files in homedir with symlinks to .dot config files
+ln -sf .dot/alias $HOME/.alias
+ln -sf .dot/bashrc $HOME/.bashrc
+ln -sf .dot/bash_profile $HOME/.bash_profile
+ln -sf .dot/gitconfig $HOME/.gitconfig
+ln -sf .dot/vimrc $HOME/.vimrc
+ln -sf ../.dot/atom_config.cson $HOME/.atom/config.cson
+
+
+# macOS configuration
+defaults write com.apple.screencapture location $HOME/Downloads
+
+defaults write com.apple.Dock autohide 1
+defaults write com.apple.Dock magnification 1
+defaults write com.apple.Dock show-recents 0
+
+defaults write com.apple.AppleMultitouchMouse MouseButtonMode TwoButton
+defaults write com.apple.AppleMultitouchMouse MouseOneFingerDoubleTapGesture 1
+
+defaults write com.apple.AppleMultitouchTrackpad Clicking 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture 0
+
+
 # Set hostname
 log "Current hostname is $HOSTNAME"
 read -p "Type the NEW hostname you want to set: " NEW_HOSTNAME
